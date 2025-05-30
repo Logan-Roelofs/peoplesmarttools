69e7e283029dabe439000de67a0c556878e3d49c
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2025-05-30 11:58:07 -0500

    init

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Button } from "../../components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function ContactUsPage() {
   return (
